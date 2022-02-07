8fab2ca8cafa23f8b40213b9bfa560f708af7529
JeanFagundes <jean-cabral@hotmail.com>
2022-02-07 00:06:47 -0300

    criado e estilizado pagina de login e começando a criar regras de autenticação

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const User = require ('./models/User')
 
 //impotar rotas
 const bankRoutes = require('./routes/bankRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 //importar controles
 const BankController = require ('./controllers/BankController')
@@ -66,6 +67,7 @@ app.use((req,res,next)=>{
 
 // routes
 app.use('/bancos', bankRoutes)
+app.use('/',authRoutes )
 
 app.get('/', BankController.showBanks)
 
